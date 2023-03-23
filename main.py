a113ebbcc1196ed62c77cc6bf053925ba4afce15
kamiltabaka <awstabaka@gmail.com>
2023-03-23 23:49:00 +0100

    Add html templates
    Added html templates for home page and history, which inherit from layout template

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
+
+
+posts = [
+    {
+        'author': 'Kamil',
+        'title': 'First test post',
+        'content': 'First test post content',
+        'date_posted': 'March 23, 2023'
+    }
+]
 
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return '<h1>Home Website</h1>'
+    return render_template("home.html", title="Home", posts=posts)
+
+
+@app.route('/history')
+def history():
+    return render_template("history.html", title="About")
